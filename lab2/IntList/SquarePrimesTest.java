a04391b77429f55e197d308287d904dec8d2979c
Young SHAW <young0723584@gmail.com>
2025-02-02 00:20:32 +0800

    debug exercise

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -1,6 +1,9 @@
 package IntList;
 
 import static org.junit.Assert.*;
+
+import afu.org.checkerframework.checker.igj.qual.I;
+import jh61b.junit.In;
 import org.junit.Test;
 
 public class SquarePrimesTest {
@@ -15,6 +18,14 @@ public class SquarePrimesTest {
         IntList lst = IntList.of(14, 15, 16, 17, 18);
         boolean changed = IntListExercises.squarePrimes(lst);
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public  void testSquarePrimesComplex(){
+        IntList lst = IntList.of(1, 2, 3, 4, 5);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 4 -> 9 -> 4 -> 25", lst.toString());
         assertTrue(changed);
     }
 }
